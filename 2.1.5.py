4d31ab469d906cfa4e0f7825e2098dc10698837b
artyom738 <mrartyomka@gmail.com>
2021-01-09 21:14:35 +0200

    решение задачи

---
+++ 2.1.5.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+import time
+import math
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+try:
+    link = "http://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+    input = browser.find_element_by_id("answer")
+    input.send_keys(y)
+    time.sleep(0.5)
+    option1 = browser.find_element_by_css_selector("[for='robotCheckbox']")
+    option1.click()
+    time.sleep(0.5)
+    option2 = browser.find_element_by_css_selector("[for='robotsRule']")
+    option2.click()
+    time.sleep(0.5)
+    button = browser.find_element_by_css_selector("[type='submit']")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(5)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
