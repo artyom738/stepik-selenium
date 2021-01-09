56e89f7dea0fb9f980ce7ffda1c7a6f413bdaf8d
artyom738 <mrartyomka@gmail.com>
2021-01-09 21:15:33 +0200

    работа с select

---
+++ 2.2.3.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import time
+
+try:
+    link = "http://suninjuly.github.io/selects2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    num1 = browser.find_element_by_id("num1")
+    num2 = browser.find_element_by_id("num2")
+    ans = int(num1.text)+int(num2.text)
+    time.sleep(0.5)
+    select = Select(browser.find_element_by_tag_name("select"))
+    select.select_by_value(str(ans))
+    button = browser.find_element_by_css_selector("[type='submit']")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(5)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
