e03d98919189ac4f36c10f626752a68fea75ec35
artyom738 <mrartyomka@gmail.com>
2021-02-05 16:20:49 +0200

    работа с фикстурами pytest

---
+++ test_fixture3.py
@@ -0,0 +1,34 @@
+import pytest
+from selenium import webdriver
+from os import system
+
+@pytest.fixture(scope="class")
+def prepare_faces():
+    print("^_^", "\n")
+    yield
+    print(":3", "\n")
+
+
+@pytest.fixture()
+def very_important_fixture():
+    print(":)", "\n")
+
+
+@pytest.fixture(autouse=True)
+def print_smiling_faces():
+    print(":-Р", "\n")
+
+
+class TestPrintSmilingFaces():
+    def test_first_smiling_faces(self, prepare_faces, very_important_fixture):
+        assert True
+
+    def test_second_smiling_faces(self, prepare_faces):
+        assert True
+
+
+
+def main():
+    system(f'pytest -s "{__file__}"')
+if __name__ == "__main__":
+    main()
